933f2fca39f6eb8d491ae464159d9cda9fe673ea
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2023-02-11 23:39:44 +0100

    Engine: [Enh] Add first physics implementation

---
+++ LeoBirds/Engine/Entity.ixx
@@ -0,0 +1,19 @@
+﻿export module Engine.Entity;
+
+namespace birds
+{
+    /**
+     * \brief Any object than can change position when time passes
+     */
+    export class Entity
+    {
+    public:
+        virtual ~Entity() = default;
+
+        /**
+         * \brief Updates the position of the object based on time
+         * \param dt The time since the last update
+         */
+        virtual void update(float dt) = 0;
+    };
+}
--- LeoBirds/main.cpp
+++ LeoBirds/main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <SFML/Window/VideoMode.hpp>
+
+#include <memory>
+
+import Sling;
+import FLib.Application;
+import FLib.Scene;
+import FLib.Layer;
+
+import Projectile;
 
 int main()
 {
-    std::cout << "Hello Leo!" << std::endl;
+    flib::Application app(sf::VideoMode(800, 600), "LeoBirds");
+
+    const auto layer = std::make_shared<flib::Layer>();
+    const auto projectile = std::make_shared<birds::Projectile>(sf::Vector2f(0, 0), "Assets/ball.jpg", 600);
+    projectile->addSpeed({50, 5});
+    layer->addDrawable(std::reinterpret_pointer_cast<sf::Drawable>(projectile));
+
+    const auto scene = std::make_shared<flib::Scene>();
+    scene->addLayer("game", layer);
+    scene->onDraw.connect(sling::Slot<float>([&projectile](const float dt)
+    {
+        projectile->update(dt);
+    }));
+
+    app.addScene("main", scene);
+    app.setActiveScene("main");
+    app.run();
+
     return 0;
 }
