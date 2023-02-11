29ad6af4c685c334f1770a1e4118d1977fa417d4
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2023-02-11 23:42:26 +0100

    Projectile: [Enh] First revision

--- LeoBirds/main.cpp
+++ LeoBirds/main.cpp
@@ -1,35 +1,7 @@
-#include <SFML/Graphics/Drawable.hpp>
-#include <SFML/System/Vector2.hpp>
-#include <SFML/Window/VideoMode.hpp>
-
-#include <memory>
-
-import Sling;
-import FLib.Application;
-import FLib.Scene;
-import FLib.Layer;
-
-import Projectile;
+#include <iostream>
 
 int main()
 {
-    flib::Application app(sf::VideoMode(800, 600), "LeoBirds");
-
-    const auto layer = std::make_shared<flib::Layer>();
-    const auto projectile = std::make_shared<birds::Projectile>(sf::Vector2f(0, 0), "Assets/ball.jpg", 600);
-    projectile->addSpeed({50, 5});
-    layer->addDrawable(std::reinterpret_pointer_cast<sf::Drawable>(projectile));
-
-    const auto scene = std::make_shared<flib::Scene>();
-    scene->addLayer("game", layer);
-    scene->onDraw.connect(sling::Slot<float>([&projectile](const float dt)
-    {
-        projectile->update(dt);
-    }));
-
-    app.addScene("main", scene);
-    app.setActiveScene("main");
-    app.run();
-
+    std::cout << "Hello Leo!" << std::endl;
     return 0;
 }
